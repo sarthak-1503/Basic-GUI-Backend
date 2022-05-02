250290d6e73f72cc363355ca9174db4d312fb68c
Sarthak Arora <63057512+sarthak-1503@users.noreply.github.com>
2022-05-02 23:23:44 +0530

    Update Home.js

--- routes/Home.js
+++ routes/Home.js
@@ -3,27 +3,21 @@ let os = require('os')
 let { PythonShell } = require('python-shell')
 let router = express.Router()
 
-// router.use(express.json())
-
 router.post('/',async(req,res) => {
     
     console.log('req dot query: ',req.query.filename)
     let {filename} = req.query;
     let Result = -1;
-    // req.body.image_data
-    // let file_path = "C:\\Users\\japni\\Desktop\\Sarthak Arora\\" + filename.toLocaleString();
+    
 
     let options = {
         mode: "text",
         pythonOptions: ["-u"],
-        // pythonPath: "C:\Users\japni\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.8\Python 3.8 (64-bit).lnk",
         scriptPath:"./",
         args: [filename]
       };
 
       console.log('options args: ',options.args);
-
-      // let pyshell = new PythonShell('Character_Recognition.py');
       
       PythonShell.run("Character_Recognition.py", options, function (err, result) {
         if (err) throw err;
@@ -32,21 +26,6 @@ router.post('/',async(req,res) => {
         res.status(200).json({success: true, result: Result});
       });
 
-      // pyshell.on('message', function(message) {
-      //   console.log(message.substr(1,message.length - 2));
-      //   Result = parseInt(message.substr(1,message.length - 2));
-      //   console.log('message+result: ', Result);
-      //   console.log('message: ', message);
-
-      //   res.status(200).json({success: true, result: Result});
-      // })
-
-      // pyshell.end(function (err,code,signal) {
-      //   if (err) throw err;
-      //   console.log('The exit code was: ' + code);
-      //   console.log('The exit signal was: ' + signal);
-      //   console.log('finished');
-      // });
 })
 
 module.exports = router;
